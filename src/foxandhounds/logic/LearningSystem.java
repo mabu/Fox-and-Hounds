73cbc7a8f5a4d3787b3d5d5e909f81697da0dd9d
Martynas Budriūnas <martynas.budriunas@gmail.com>
2012-05-04 14:16:49 +0200

    Added discount factor and methods for finding greedy Q-values.

--- src/foxandhounds/logic/LearningSystem.java
+++ src/foxandhounds/logic/LearningSystem.java
@@ -6,11 +6,14 @@ package foxandhounds.logic;
 abstract public class LearningSystem {
     protected double explorationRate;
     protected double learningRate;
+    protected double discountFactor;
     protected static final int numStates = 32 * 31 * 30 * 29 * 28 / 4 / 3 / 2;
 
-    public LearningSystem(double explorationRate, double learningRate) {
+    public LearningSystem(double explorationRate, double learningRate,
+                          double discountFactor) {
         this.explorationRate = explorationRate;
         this.learningRate = learningRate;
+        this.discountFactor = discountFactor;
     }
 
     public void setExplorationRate(double explorationRate) {
@@ -18,8 +21,32 @@ abstract public class LearningSystem {
     }
 
     public void setLearningRate(double learningRate) {
-        this.learningRate = explorationRate;
+        this.learningRate = learningRate;
+    }
+
+    public void setDiscountFactor(double discountFactor) {
+        this.discountFactor = explorationRate;
     }
 
     abstract public State move(State state);
+
+    protected int greedyAction(double[] qValues) {
+        int action = 0;
+        for (int i = 1; i < qValues.length; ++i) {
+            if (qValues[i] < qValues[action]) {
+                action = i;
+            }
+        }
+        return action;
+    }
+
+    protected int qMax(double[] qValues) {
+        int action = 0;
+        for (int i = 1; i < qValues.length; ++i) {
+            if (qValues[i] < qValues[action]) {
+                action = i;
+            }
+        }
+        return action;
+    }
 }
