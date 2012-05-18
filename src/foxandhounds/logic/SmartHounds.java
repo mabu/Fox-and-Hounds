f08597d0c7f43563242b7f6ee7d614089cd02b40
Martynas Budriūnas <martynas.budriunas@gmail.com>
2012-05-18 17:26:19 +0200

    Added variants of Fox and Hounds which adjust exploration rate based on their recent results.

---
+++ src/foxandhounds/logic/SmartHounds.java
@@ -0,0 +1,56 @@
+package foxandhounds.logic;
+
+import java.io.Serializable;
+import java.util.LinkedList;
+
+/**
+ * Smart hounds' learning system.
+ *
+ * The better it plays, the less it explores.
+ */
+public class SmartHounds extends Hounds implements Serializable, Cloneable {
+    private static final int RECENT_RESULTS_LENGTH = 100;
+    private LinkedList<Boolean> recentResults = new LinkedList<Boolean>();
+    private int recentWins;
+
+    public SmartHounds(double explorationRate, double learningRate,
+               double discountFactor) {
+        super(explorationRate, learningRate, discountFactor);
+    }
+
+    /**
+     * Calculates a reward given for a given state.
+     *
+     * @param state a state to evaluate
+     * @return a reward value
+     */
+    protected double reward(State state) {
+        if (state.houndsWon()) {
+            ++wins;
+            ++recentWins;
+            recentResults.offer(true);
+            updateExplorationRate();
+            return 1;
+        } else if (state.foxWon()) {
+            ++losses;
+            recentResults.offer(false);
+            updateExplorationRate();
+            return -1;
+        } else {
+            return 0;
+        }
+    }
+
+    private void updateExplorationRate() {
+        if (recentResults.size() > RECENT_RESULTS_LENGTH) {
+            if (recentResults.poll()) {
+                --recentWins;
+            }
+        }
+        setExplorationRate(0.5 - wins / (double)recentResults.size());
+    }
+
+    public Object clone() {
+        return super.clone();
+    }
+}
