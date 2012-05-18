ea4ecbcc720105d44079880fb2ded5dbd48ffb1c
Martynas Budriūnas <martynas.budriunas@gmail.com>
2012-05-18 16:01:22 +0200

    Multithreaded experiments.

--- src/foxandhounds/logic/Experiment.java
+++ src/foxandhounds/logic/Experiment.java
@@ -11,23 +11,26 @@ import java.io.FileNotFoundException;
  * During the evaluation phase, exploration and learning rates are set to 0 and
  * a given number of games are played.
  */
-public class Experiment {
+public class Experiment implements Runnable {
     private Fox fox;
     private Hounds hounds;
     private State state;
-    private int trainingTurns;
-    private int evaluationGames;
+    private long trainingTurns;
+    private long evaluationGames;
+    private int cyclesLeft;
+    private PrintStream out;
+    private boolean isRunning = false;
 
     /**
-     * Initializes an expirement.
+     * Initializes an experiment.
      *
-     * @param fox a fox to expirement with
-     * @param hounds hounds to expirement with
+     * @param fox a fox to experiment with
+     * @param hounds hounds to experiment with
      * @param trainingTurns the number of turns per training phase
      * @param evaluationGames the number of games played to evaluate
      */
-    public Experiment(Fox fox, Hounds hounds, int trainingTurns,
-                      int evaluationGames) {
+    public Experiment(Fox fox, Hounds hounds, long trainingTurns,
+                      long evaluationGames) {
         this.fox = fox;
         this.hounds = hounds;
         this.trainingTurns = trainingTurns;
@@ -36,7 +39,7 @@ public class Experiment {
     }
 
     /**
-     * Runs some cycles of an expirement.
+     * Runs some cycles of an experiment.
      * 
      * Each cycle consists of a training phase followed by an evaluation phase.
      * The results are printed as the number of fox and hounds wins separated
@@ -44,21 +47,31 @@ public class Experiment {
      *
      * @param cycles the number of cycles to run the experiment
      * @param out output stream
+     * @return the thread which runs the experiment
      */
-    public void run(int cycles, PrintStream out) {
-        while (cycles-- > 0) {
+    public synchronized Thread run(int cycles, PrintStream out) {
+        cyclesLeft = cycles;
+        this.out = out;
+        Thread thread = new Thread(this);
+        thread.start();
+        return thread;
+    }
+
+    public synchronized void run() {
+        isRunning = true;
+        while (cyclesLeft-- > 0) {
             for (int i = 0; i < trainingTurns; ++i) {
                 state = step(fox, hounds, state);
             }
             Fox evaluationFox = (Fox)fox.clone();
             Hounds evaluationHounds = (Hounds)hounds.clone();
-            State evaluationState = evaluationFox.startGame();
-            long initFoxWins = evaluationFox.getWins();
-            long initHoundsWins = evaluationHounds.getWins();
             evaluationFox.setLearningRate(0);
             evaluationFox.setExplorationRate(0);
             evaluationHounds.setLearningRate(0);
             evaluationHounds.setExplorationRate(0);
+            State evaluationState = evaluationFox.startGame();
+            long initFoxWins = evaluationFox.getWins();
+            long initHoundsWins = evaluationHounds.getWins();
             while (evaluationFox.getWins() + evaluationFox.getLosses()
                    - initFoxWins - initHoundsWins < evaluationGames) {
                 evaluationState = step(evaluationFox, evaluationHounds,
@@ -66,7 +79,9 @@ public class Experiment {
             }
             out.println((evaluationFox.getWins() - initFoxWins) + " "
                         + (evaluationHounds.getWins() - initHoundsWins));
+            out.println(evaluationFox.getWins() + " " + initFoxWins + " " + evaluationHounds.getWins() + " " + initHoundsWins + " " + evaluationGames);
         }
+        isRunning = false;
     }
 
     private State step(Fox fox, Hounds hounds, State state) {
@@ -88,45 +103,61 @@ public class Experiment {
      * In this experiment we train two foxes and two instances of hounds with
      * different learning rates against random opponents. Then we make them play
      * against each other.
+     * 
+     * @param cycles the number of cycles for the experiments
      */
-    private static void expirement1() {
+    private static void experiment1(int cycles) {
         Fox fox1 = new Fox(0.1, 0.5, 0.99);
         Fox fox2 = new Fox(0.1, 0.3, 0.99);
-        Fox randomFox = new Fox(0, 0, 0);
+        Fox randomFox1 = new Fox(0, 0, 0);
+        Fox randomFox2 = new Fox(0, 0, 0);
         Hounds hounds1 = new Hounds(0.1, 0.5, 0.99);
         Hounds hounds2 = new Hounds(0.1, 0.3, 0.99);
-        Hounds randomHounds = new Hounds(0, 0, 0);
+        Hounds randomHounds1 = new Hounds(0, 0, 0);
+        Hounds randomHounds2 = new Hounds(0, 0, 0);
 
         try {
-            Experiment experiment;
-            experiment = new Experiment(fox1, randomHounds, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/fox1VSrandom"));
-            System.out.println("Done 1");
-            experiment = new Experiment(fox2, randomHounds, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/fox2VSrandom"));
-            System.out.println("Done 2");
-            experiment = new Experiment(randomFox, hounds1, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds1VSrandom"));
-            System.out.println("Done 3");
-            experiment = new Experiment(randomFox, hounds2, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds2VSrandom"));
-            System.out.println("Done 4");
-            experiment = new Experiment(fox1, hounds1, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds1VSfox1"));
-            System.out.println("Done 5");
-            experiment = new Experiment(fox1, hounds2, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds2VSfox1"));
-            System.out.println("Done 6");
-            experiment = new Experiment(fox2, hounds1, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds1VSfox2"));
-            System.out.println("Done 7");
-            experiment = new Experiment(fox2, hounds2, 100000, 100);
-            experiment.run(100, new PrintStream("expirement1/hounds2VSfox2"));
-            System.out.println("Done 8");
-        } catch (FileNotFoundException ignored) { }
+            Experiment e1 = new Experiment(fox1, randomHounds1, 100000, 100);
+            Thread t1 = e1.run(cycles,
+                               new PrintStream("experiment1/fox1VSrandom"));
+            Experiment e2 = new Experiment(fox2, randomHounds2, 100000, 100);
+            Thread t2 = e2.run(cycles,
+                               new PrintStream("experiment1/fox2VSrandom"));
+            Experiment e3 = new Experiment(randomFox1, hounds1, 100000, 100);
+            Thread t3 = e3.run(cycles,
+                               new PrintStream("experiment1/hounds1VSrandom"));
+            Experiment e4 = new Experiment(randomFox2, hounds2, 100000, 100);
+            Thread t4 = e4.run(cycles,
+                               new PrintStream("experiment1/hounds2VSrandom"));
+            t1.join();
+            t2.join();
+            t3.join();
+            t4.join();
+            System.out.println("Done training.");
+            Experiment e5 = new Experiment(fox1, hounds1, 100000, 100);
+            Thread t5 = e5.run(cycles,
+                               new PrintStream("experiment1/fox1VShounds1"));
+            Experiment e6 = new Experiment(fox2, hounds2, 100000, 100);
+            Thread t6 = e6.run(cycles,
+                               new PrintStream("experiment1/fox2VShounds2"));
+            t5.join();
+            t6.join();
+            System.out.println("Done first round.");
+            Experiment e7 = new Experiment(fox1, hounds2, 100000, 100);
+            Thread t7 = e7.run(cycles,
+                               new PrintStream("experiment1/fox1VShounds2"));
+            Experiment e8 = new Experiment(fox2, hounds1, 100000, 100);
+            Thread t8 = e8.run(cycles,
+                               new PrintStream("experiment1/fox2VShounds1"));
+            t7.join();
+            t8.join();
+            System.out.println("Done second round.");
+        }
+        catch (FileNotFoundException ignored) { }
+        catch (InterruptedException ignored) { }
     }
 
     public static void main(String args[]) {
-        expirement1();
+        experiment1(100);
     }
 }
