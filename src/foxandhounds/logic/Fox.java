b0e9ed5e1cfc23e48bb851dd31dde7119fd0bb37
Martynas Budriūnas <martynas.budriunas@gmail.com>
2012-05-18 12:02:02 +0200

    Fox and hounds cannot win both anymore.

--- src/foxandhounds/logic/Fox.java
+++ src/foxandhounds/logic/Fox.java
@@ -1,32 +1,30 @@
-/**
- * Fox's learning system.
- */
 package foxandhounds.logic;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.Vector;
 
+/**
+ * Fox's learning system.
+ *
+ * Fox may go to up to 4 directions: up left, up right, down left, down
+ * right.
+ *
+ * There is a special state when the fox has not yet chosen its
+ * initial position. Its index is State.NUM_STATES - 1 and the i-th action
+ * is to choose i-th column as a starting position.
+ */
 public class Fox extends LearningSystem implements Serializable{
-    /*
-     * Fox may go to up to 4 directions: up left, up right, down left, down
-     * right. There is also a special state when the fox has not yet chosen its
-     * initial position. Its index is State.NUM_STATES - 1 and the i-th action
-     * is to choose i-th column as a starting position.
-     */
 
     public Fox(double explorationRate, double learningRate,
-            double discountFactor) {
+               double discountFactor) {
         super(explorationRate, learningRate, discountFactor);
         qValues = new double[State.NUM_STATES][4];
     }
 
     /**
-     * Returns Q-values of given state. The Q-values are given for fox moves in
-     * that order: up left, up right, down left, down right.
+     * Returns Q-values of given state.
+     * The Q-values are given for fox moves in that order: up left, up right,
+     * down left, down right.
      *
      * @param state Q-values of given state will be returned
      * @return array of Q-values, in the form described above
@@ -46,7 +44,8 @@ public class Fox extends LearningSystem implements Serializable{
     }
 
     /**
-     * Initialize the game. The fox chooses initial position.
+     * Initialize the game.
+     * The fox chooses initial position.
      *
      * @return initial game state
      */
@@ -65,16 +64,15 @@ public class Fox extends LearningSystem implements Serializable{
      * @return a reward value
      */
     protected double reward(State state) {
-        if (state.foxWon()) {
-            ++wins;
-            return 1;
-        } else if (state.houndsWon()) {
+        if (state.houndsWon()) {
             ++losses;
             return -1;
+        } else if (state.foxWon()) {
+            ++wins;
+            return 1;
         } else {
             return 0;
         }
     }
 
-    
 }
